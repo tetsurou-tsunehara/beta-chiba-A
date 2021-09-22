d38db4942a13fc509c0a12545480961d474c7018
tetsurou-tsunehara <tetsurou_19n1101087@nnn.ed.jp>
2021-09-22 12:15:52 +0900

    完成

--- assets/js/hint.js
+++ assets/js/hint.js
@@ -7,5 +7,6 @@ const img = ['9', '2', '1', '8', '7', '6'];
 for (let i = 1; i <= 9; i++) {
   if (cookies.includes(`stage${i}=1`)) {
     $(`#${img[i - 1]}`).css('opacity', '1');
+    $(`#stage${i}`).removeClass('shadow');
   }
 }
